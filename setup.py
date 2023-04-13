1a67f7c8847f8138c88b8e6c4819a1361323eab0
Mihir Desai <madesai98@outlook.com>
2023-04-12 22:36:13 -0700

    Initial project setup.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maxxx-inferno',
+    version='0.0.1',
+    description='A command-line tool designed to easily set up a flexible and scalable backend using FastAPI and Firebase.',
+    author='Mihir Desai',
+    author_email='madesai98@outlook.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'inferno=inferno:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.7',
+)
